72c7f5122c0e3e33647c33d3f1b7982eca78cb39
Dziana Kavaleva <dianakove32@gmail.com>
2021-03-13 12:23:12 +0300

    add mixins

--- src/components/Pages/MainPage/AboutSection.jsx
+++ src/components/Pages/MainPage/AboutSection.jsx
@@ -4,14 +4,17 @@ import './AboutSection.scss'
 export default function AboutSection() {
     return (
         <div className='AboutSection'>
-        <div className='container'>
+        <div className='container-aboutSection'>
 <div className='content-container'>
 <h2>Возьму животное</h2>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.
 Сommodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
 </div>
-<img src={dog} alt="dog"/>
+<div>
+    <img src={dog} alt="dog"/>
+</div>
+
         </div>
 
         </div>
