7fe39ea4aed8d05d8758b49a9c5d2b2ee20e9e69
Otkrovennost <Otkrovennost@mail.ru>
2021-03-13 18:27:17 +0300

    fixed img

--- src/components/Pages/MainPage/MainPage.jsx
+++ src/components/Pages/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 import "./MainPage.scss"
-import Pict from '../../../assets/image.png'
+import Girl from '../MainPage/assets/girl.png'
 import Logo from '../../../assets/Logo.svg'
 import AboutSection from './AboutSection';
 
@@ -14,14 +14,9 @@ const MainPage = () => {
         <div className='logo'>
           <img src={Logo} alt="logo"/>
         </div>
-
         <div className='wrapper-content container'>
-          <div><img className="wrapper-content__img" src={Pict} alt="funny girl"/></div>
-
-
-
+          <img className="wrapper-content__img" src={Girl} alt="funny girl"/>
           <div className='section'>
-
             <h1>Home Pet Hottel</h1>
             <p>your pet will feel itself right at home</p>
             <div className='button-general'>
@@ -31,8 +26,6 @@ const MainPage = () => {
           </div>
         </div>
       </div>
-
-
       <AboutSection />
     </div >
   )
